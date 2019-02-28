f0804832638178f01bd22f8907660e361e555a1f
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2019-02-28 09:58:38 -0700

    Update findaadupninlocalad.ps1

--- findaadupninlocalad.ps1
+++ findaadupninlocalad.ps1
@@ -2,7 +2,7 @@
 #Require -module activedirectory
 <#PSScriptInfo
 
-.VERSION 0.6
+.VERSION 0.10
 
 .GUID 5e7bfd24-88b8-4e4d-99fd-c4ffbfcf5be6
 
@@ -29,67 +29,56 @@ from the use or distribution of the Sample Code..
 .description
 
 #>
-Param($reportpath = "$env:userprofile\Documents")
+Param($days=15,$reportpath = "$env:userprofile\Documents")
 Connect-MsolService
-cls
-$mismatched_users = @()
+$changes_in_days = [DateTime]::Today.AddDays(-$days) 
+$changes_in_days = (get-date).Adddays(-($days))
 
-function searchforupn{
+function searchAADforUPN{
     param($upn)
-    foreach($adupn in $adusers_upns){
-        if($aadupn -eq $adupn){
-            Return $true
-            exit
-        }
+    if(get-msoluser -UserPrincipalName $upn){
+        Write-host "$upn found in Azure AD"
+        $true
+    }else{
+        $false
+        Write-host "$upn not found in Azure AD"
     }
-    return $false
 }
-function searchforproxy{
-    param($upn)
-    foreach($aduser in $adusers){
-        if($aduser.proxyaddress -like "*$aadupn*"){
-            Return $aduser
-            exit
-        }
-    }
-    $upn | select @{name="userprincipalname";Expression={$upn}}, `
-            @{name="proxyaddress";Expression={"Not Found"}}
-}
-
-Write-host "Gathering Synced Licensed Users from AAD"
-$aadusers_upns = (Get-MsolUser -Synchronized -all | where {$_.isLicensed -eq $true} | select Userprincipalname).Userprincipalname
-#$aadusers_upns = (Get-MsolUser -Synchronized -all | select Userprincipalname).Userprincipalname
-[System.Collections.ArrayList]$adusers = @()
-[System.Collections.ArrayList]$adusers_upns = @()
-Write-host "Gathering Users from AD"
-foreach($domain in (get-adforest).domains){
-    Write-host "Gathering users from $domain"
-    get-aduser -ldapfilter "(proxyaddresses=*)" -server $domain -pipelinevariable aduser `
-        -properties proxyaddresses | foreach{
-            $adusers_upns.Add($(($aduser).userprincipalname))
-           $aduser | select -ExpandProperty proxyaddresses -PipelineVariable pa | foreach {
-               $adusers.Add($($aduser | select userprincipalname, `
-                @{name="proxyaddress";Expression={$pa}}))
+$time = Measure-Command -Expression { `
+$ad_users = get-adforest | select -ExpandProperty domains -PipelineVariable domain | foreach{
+    Write-host "Collecting Users from $domain"
+    get-aduser -ldapfilter "(proxyaddresses=*)" `
+        -server $domain -properties "msDS-ReplAttributeMetaData",userprincipalname,proxyaddresses | select `
+        @{name='Domain';expression={$domain}},userprincipalname,samaccountname,"msDS-ReplAttributeMetaData",proxyaddresses
+}} | select minutes
+            
+Write-host "AD Query Time $(($time).minutes) minutes"
+Write-host "Filtering out objects with no upn or proxy change in last $changes_in_days days"
+$time = Measure-Command -Expression { `
+    $ad_users_changed = $ad_users | select domain,userprincipalname,proxyaddresses, `
+            @{name='ProxyAddressChangeDate';expression={($_ | `
+                Select-Object -ExpandProperty "msDS-ReplAttributeMetaData" | foreach {([XML]$_.Replace("`0","")).DS_REPL_ATTR_META_DATA |`
+                where { $_.pszAttributeName -eq "proxyaddresses"}}).ftimeLastOriginatingChange | get-date}}, `
+            @{name='UPNChangeDate';expression={($_ | `
+                Select-Object -ExpandProperty "msDS-ReplAttributeMetaData" | foreach {([XML]$_.Replace("`0","")).DS_REPL_ATTR_META_DATA |`
+                where { $_.pszAttributeName -eq "UserPrincipalname"}}).ftimeLastOriginatingChange | get-date}} | `
+                where {($_.UPNChangeDate -gt $changes_in_days) -or ($_.ProxyAddressChangeDate -gt $changes_in_days)}
+} | select minutes
+ Write-host "UPN Filter Time $(($time).minutes) minutes"
+ write-host "Found $(($ad_users_changed  | measure-Object).count) Users with UPN Changed in last $changes_in_days days"
+ Write-host "Validate Objects exist in Azure AD"
+ $results = @()
+ foreach($adu in $ad_users_changed){
+    if(!(searchAADforUPN -upn ($adu).userprincipalname)){
+        $adu | select -ExpandProperty proxyaddresses -PipelineVariable proxy | foreach {
+            if(searchAADforUPN -upn $(($proxy -split ":")[1])){
+                $results += $adu | select userprincipalname, UPNChangeDate,ProxyAddressChangeDate, `
+                            @{name='AADUPN';expression={$(($proxy -split ":")[1])}}
             }
         }
-}
-
-
-write-host "Creating Unique List of UPNs for AD Users"
-$adusers = $adusers | sort
-$adusers_upns = $adusers_upns | sort
-$aadusers_upns = $aadusers_upns | sort
-write-host "Retrieved $(($aadusers_upns | Measure-Object).count) UPNs from AAD"
-write-host "Retrieved $(($adusers | Measure-Object).count) Unique Proxy Entries from AD"
-write-host "Retrieved $(($adusers_upns | Measure-Object).count) Unique UPN from AD"
-
-
-foreach($aadupn in $aadusers_upns){
-    if(!(searchforupn -upn $aadupn)){
-        Write-host "UPN Not Found: $aadupn"
-        $mismatched_users += searchforproxy -upn $aadupn
     }
-}
+ }
+ 
 
-$mismatched_users | export-csv "$reportpath\aaduserswithmismatchedupn.csv" -NoTypeInformation
-write-host "Found Users with Mismatched UPN in AD Total: $(($mismatched_users | Measure-Object).count)"
+ $ad_users_changed | select domain,userprincipalname,ProxyAddressChangeDate,UPNChangeDate  | export-csv "$reportpath\upnchanged.csv" -NoTypeInformation
+ $results | export-csv "$reportpath\upnnotfoundinaad.csv" -NoTypeInformation
