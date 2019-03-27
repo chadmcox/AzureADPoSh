230ceb38498e8144d22f3c19112dd7681ab94473
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2019-03-27 15:35:45 -0700

    Update ExportAADAppConsents.ps1

--- ExportAADAppConsents.ps1
+++ ExportAADAppConsents.ps1
@@ -1,7 +1,7 @@
 #requires -module azureadpreview
 <#PSScriptInfo
 
-.VERSION 0.5
+.VERSION 0.6
 
 .GUID 0e98504a-1173-4af8-a6ab-9564fdbadfa5
 
@@ -25,6 +25,8 @@ from the use or distribution of the Sample Code..
 
 .DESCRIPTION 
 https://docs.microsoft.com/en-us/graph/api/resources/oauth2permissiongrant?view=graph-rest-beta
+
+Added the
 #>
 Param($reportpath = "$env:userprofile\Documents")
 
@@ -32,40 +34,37 @@ Param($reportpath = "$env:userprofile\Documents")
 $hash_ignore = @{Name="Ignore";Expression={if(($AADSP.serviceprincipaltype -eq "legacy") -or `
     ($AADSP.PublisherName -like "Microsoft*") -or ($AADSP.ServicePrincipalType -eq "ManagedIdentity")){$true}else{$False}}}
 
-connect-azuread
-
+#connect-azuread
+cls
 #retrieve up to two permissiongrants from all applications
 
-    Write-host "Collecting Application Data from Azure AD. This could take a really long time." 
-    try{$application_service_principals = Get-AzureADServicePrincipal -All $true}
-    catch{throw $_.Exception}
-
-    Write-host "Collecting Consent Data for each application from Azure AD. This will take a while"
-    $application_service_principal_consents = foreach($aadsp in $application_service_principals){
-        write-host "Gathering info from $($AADSP.Displayname)"
-        $aadsp | Get-AzureADServicePrincipalOAuth2PermissionGrant -top 2 -PipelineVariable PERMGrant |  select `
-                @{Name="ServicePrincipalDisplayName";Expression={$AADSP.Displayname}}, `
-                @{Name="ServicePrincipalObjectID";Expression={$AADSP.ObjectID}}, `
-                @{Name="ServicePrincipalObjectType";Expression={$AADSP.ObjectType}}, `
-                @{Name="AppDisplayName";Expression={$AADSP.AppDisplayName}}, `
-                @{Name="AppId";Expression={$AADSP.AppId}}, `
-                @{Name="PublisherName";Expression={$AADSP.PublisherName}}, `
-                @{Name="AccountEnabled";Expression={$AADSP.AccountEnabled}}, `
-                @{Name="ServicePrincipalType";Expression={$AADSP.ServicePrincipalType}},`
-                ConsentType,ExpiryTime,PrincipalId,Scope,StartTime,$hash_ignore
-    }
-    Write-host "Building Summary of Data"
-    $summary = $application_service_principal_consents | where ignore -eq $false | select -expandproperty ServicePrincipalObjectID -Unique -PipelineVariable SpObjID | foreach{
-        $all_grants_per_sp = $results | where ServicePrincipalObjectID -eq $SpObjID
-        $all_grants_per_sp | select -First 1 | select ServicePrincipalDisplayName, ServicePrincipalObjectID, `
-            ServicePrincipalObjectType, AppDisplayName, AppId, PublisherName, AccountEnabled, `
-            ServicePrincipalType,ConsentType,ExpiryTime,Scope, `
+Write-host "Collecting Application Service Principals from AD." 
+$time_to_run = measure-command {try{$application_service_principals = Get-AzureADServicePrincipal -Filter "serviceprincipaltype eq 'Application'" -All $true} 
+catch{throw $_.Exception}}
+Write-host "Completed $($time_to_run.minutes) minutes"
+$count = $application_service_principals.count; $i = 1
+Write-host "Collecting Consent Data for each application from Azure AD. This will take a while"
+$time_to_run = measure-command {$summary = $application_service_principals | select -PipelineVariable aadsp | foreach { 
+    write-host "$i of $count : Gathering Consent for $($AADSP.AppDisplayName)"; $i++
+    $all_grants_per_sp =  $aadsp | Get-AzureADServicePrincipalOAuth2PermissionGrant -top 2 -PipelineVariable PERMGrant
+    $all_grants_per_sp | select @{Name="ServicePrincipalDisplayName";Expression={$AADSP.Displayname}}, `
+        @{Name="ServicePrincipalObjectID";Expression={$AADSP.ObjectID}}, `
+        @{Name="ServicePrincipalObjectType";Expression={$AADSP.ObjectType}}, `
+        @{Name="AppDisplayName";Expression={$AADSP.AppDisplayName}}, `
+        @{Name="AppId";Expression={$AADSP.AppId}}, `
+        @{Name="PublisherName";Expression={$AADSP.PublisherName}}, `
+        @{Name="AccountEnabled";Expression={$AADSP.AccountEnabled}}, `
+        @{Name="ServicePrincipalType";Expression={$AADSP.ServicePrincipalType}},` 
+            ConsentType,ExpiryTime,Scope, `
             @{Name="ConsentCount";Expression={ `
-                if(($all_grants_per_sp | measure-object).count -eq 1 -and $_.ConsentType -eq "Principal")
-                    {"Single"}elseif($_.ConsentType -eq "Principal"){"Multiple"}}}
-    }
+            if(($all_grants_per_sp | measure-object).count -eq 1 -and $_.ConsentType -eq "Principal")
+                {"Single"}elseif($_.ConsentType -eq "Principal"){"Multiple"}}} -First 1 
+}}
 
+cls
+Write-host "Completed $($time_to_run.minutes) minutes"
 
+write-host "-----"
 Write-host "Consented_by_Admin: $(($summary | where ConsentType -eq "AllPrincipals" | measure-object).count)"
 Write-host "Consented_by_Single_User: $(($summary | where ConsentCount -eq "Single" | measure-object).count)"
 Write-host "Consented_by_Multiple_Users: $(($summary | where ConsentCount -eq "Multiple" | measure-object).count)"
