6f2cec7fb9c99869af64d8b9e1f06ef1360f6919
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-04-16 08:46:00 -0700

    Update collectAzureSubRoles.ps1

--- collectAzureSubRoles.ps1
+++ collectAzureSubRoles.ps1
@@ -1,7 +1,8 @@
-
+#Requires -modules Az.Accounts,Az.Resources 
+#Requires -version 4.0
 <#PSScriptInfo
 
-.VERSION 0.1
+.VERSION 2020.4.16
 
 .GUID 476739f9-d907-4d5a-856e-71f9279955de
 
@@ -25,69 +26,141 @@ and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and
 against any claims or lawsuits, including attorneys` fees, that arise or result
 from the use or distribution of the Sample Code..
 
-.TAGS Get-AzureRmContext Set-AzureRmContext Get-AzureRmRoleAssignment
-
-.LICENSEURI 
-
-.PROJECTURI 
-
-.ICONURI 
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS 
-
-.EXTERNALSCRIPTDEPENDENCIES 
-
-.RELEASENOTES
-
-
-#>
-
-#Requires -Module azurerm
-
-<# 
-
 .DESCRIPTION 
- Ensure that multi-factor authentication is enabled for all Azure Subscription privileged users 
+ retrieves all objects and  
 
 #> 
-Param()
+param($reportpath="$env:userprofile\Documents")
+$report = "$reportpath\Azure_Resource_to_Account_Mapping_$(get-date -f yyyy-MM-dd-HH-mm).csv"
 
-function getAzureSubRBACRoles{
-    read-host "Press enter, then login with Account  to query Azure Services"
-    Connect-AzureRmAccount
-    Get-AzureRmContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzureRmContext | foreach{
-        $subinfo = $AzureRMSub.name.split("-")
-        write-Information "Gathering Infromation from $($AzureRMSub.id)"
-        Get-AzureRmRoleDefinition -Custom -PipelineVariable ASUBRDEF | foreach{
-            $ASUBRDEF.Actions | select `
-                @{Name="SubscriptionID";Expression={$AzureRMSub.Subscription}},`
-                @{Name="SubscriptionName";Expression={$subinfo[0]}},`
-                @{Name="RoleName";Expression={$ASUBRDEF.Name}}, `
-                @{Name="RoleID";Expression={$ASUBRDEF.ID}}, `
-                @{Name="isCustom";Expression={$ASUBRDEF.isCustom}}, `
-                @{Name="Action";Expression={$_}}, `
-                @{Name="AssignableScopes";Expression={$ASUBRDEF.AssignableScopes}}
-                @{Name="Result";Expression={if($ASUBRDEF.AssignableScopes -eq '/' -and
-                    $ASUBRDEF.isCustom -eq $true -and $ASUBRDEF.isCustom -eq '*')
-                    {"Review"}else{"Success"}}
-        }
-        Get-AzureRmRoleDefinition -PipelineVariable ASUBRDEF | foreach{
-            $ASUBRDEF.Actions | select `
-                @{Name="SubscriptionID";Expression={$AzureRMSub.Subscription}},`
-                @{Name="SubscriptionName";Expression={$subinfo[0]}},`
-                @{Name="RoleName";Expression={$ASUBRDEF.Name}}, `
-                @{Name="RoleID";Expression={$ASUBRDEF.ID}}, `
-                @{Name="isCustom";Expression={$ASUBRDEF.isCustom}}, `
-                @{Name="Action";Expression={$_}}, `
-                @{Name="AssignableScopes";Expression={$ASUBRDEF.AssignableScopes}}
-                @{Name="Result";Expression={$ASUBRDEF.AssignableScopes}}
-                @{Name="Result";Expression={if($ASUBRDEF.AssignableScopes -eq '/' -and
-                    $ASUBRDEF.isCustom -eq $true -and $ASUBRDEF.isCustom -eq '*')
-                    {"Review"}else{"Success"}}
-        }
+
+function resolveAzureRoleObject{
+    [cmdletbinding()]
+    param($name,$objecttype)
+    if($objecttype -eq "User"){
+        (get-azaduser -userprincipalname $name).id
     }
 }
 
-getAzureSubRBACRoles
+function expandAzureADGroup{
+    [cmdletbinding()]
+    param($objectid)
+    Get-AzADGroupMember -GroupObjectId $objectid -PipelineVariable azgm | foreach{
+        if($_.ObjectType -eq "Group"){
+            expandAzureADGroup -objectid $_.id
+        }else{
+        $_ | select `
+            @{Name="Signinname";Expression={$_.UserPrincipalName}}, `
+            @{Name="DisplayName";Expression={$_.DisplayName}}, `
+            @{Name="ObjectID";Expression={$_.ID}}, `
+            ObjectType
+        }
+    }
+}
+function collectAzureSubscriptions{
+    [cmdletbinding()]
+    param()
+    #Get-AzContext -ListAvailable | where {$_.Subscription}
+    Get-AzSubscription
+}
+function Launcher{
+    $availableazsubscriptions = collectAzureSubscriptions
+    cls
+    Write-host -ForegroundColor yellow "Select the Subscription you want to query:"
+    $i = 0
+    #$availableazsubscriptions | where {$_.Subscription} | foreach{
+    $availableazsubscriptions | foreach{
+        write-host "$i - $($_.name)"; $i++
+    }
+    write-host "$i - All of the Above"
+    $option = read-host "Please enter a number 0 to $i..."
+    if($option -eq $i){
+        $availableazsubscriptions | foreach{
+            write-host "Collecting roles for $($_.SubscriptionId)" -ForegroundColor Yellow
+            collectAZRoles -subid $_.SubscriptionId
+        }
+    }else{
+        write-host "Collecting roles for $($availableazsubscriptions[$option].name)" -ForegroundColor Yellow
+        collectAZRoles -subid $availableazsubscriptions[$option].Subscription, -subname $availableazsubscriptions[$option].name
+    }
+}
+function collectAZRoles{
+    [cmdletbinding()]
+    param($subid,$subname)
+    write-host "Connecting to $subid" -ForegroundColor Yellow
+    try{set-azcontext -Subscription $subid -ErrorAction Continue}
+        catch{write-host "unable to connect to $subid" -ForegroundColor Red}
+    write-host "Retrieving resources $($subid)" -ForegroundColor Yellow
+    $azure_resources = get-azresource | group ResourceId -AsHashTable -AsString
+    Get-AzRoleAssignment -IncludeClassicAdministrators -PipelineVariable azroas | foreach{
+        foreach($azrole in $($azroas.RoleDefinitionName -split ";")){
+            if($_.ObjectType -eq "Group"){
+                expandAzureADGroup -objectid $azroas.ObjectID -PipelineVariable azadg | select `
+                    @{Name="Group";Expression={$azroas.Displayname}}, `
+                    @{Name="Scope";Expression={$azroas.scope}}, `
+                    @{Name="RoleDefinitionName";Expression={$azrole}}, `
+                    Displayname,signinname,objectid,ObjectType
+            }else{
+                $_ | select @{Name="Group";Expression={}}, `
+                    @{Name="RoleDefinitionName";Expression={$azrole}}, `
+                    Displayname,signinname,scope, `
+                    @{Name="ObjectID";Expression={if($azroas.ObjectID){$azroas.ObjectID}else{resolveAzureRoleObject -name $azroas.SignInName -objecttype $azroas.objecttype}}}, `
+                    ObjectType
+            }
+        }
+    } | select @{N="Subscription";E={if(($_.scope -split "/")[1] -eq "subscriptions"){($_.scope -split "/")[2]}else{$subid}}}, `
+        @{N="SubscriptionName";E={$subname}}, `
+        @{N="ResourceGroup";E={if(($_.scope -split "/")[3] -eq "resourceGroups"){($_.scope -split "/")[4]}}}, `
+        @{N="Provider";E={if(($_.scope -split "/")[5] -eq "providers"){($_.scope -split "/")[6]}}}, `
+        @{N="Resource";E={if(($_.scope -split "/").count -gt 5){($_.scope -split "/")[-1]}}}, `
+        scope,RoleDefinitionName, `
+        group, ObjectID,ObjectType, Displayname, Signinname | export-csv $report -NoTypeInformation -Append
+    write-host "Finished with $subid" -ForegroundColor Yellow
+}
+
+<#Function collectAzureRoles{
+    [cmdletbinding()]
+    param()
+    #Collect Azure Resource Roles
+           
+
+    $file = "$working_directory\$($file_prefix)_t_azure_resources.tsv" 
+    Get-AzContext -ListAvailable -PipelineVariable azco | set-azcontext | foreach{
+        get-azresource | select `
+        @{Name="subscriptionName";Expression={$azco.name}}, `
+            @{Name="SubscriptionID";Expression={$azco.subscription}}, `
+            @{Name="TenantId";Expression={$Azco.Tenant}}, `
+            resourceid, resourcename, resourcetype, ResourceGroupName
+    } 
+    Get-AzContext -ListAvailable -PipelineVariable azco | set-azcontext | foreach{
+        Get-AzRoleAssignment -IncludeClassicAdministrators -PipelineVariable azroas | foreach{
+        foreach($azrole in $($azroas.RoleDefinitionName -split ";")){
+            if($_.ObjectType -eq "Group"){
+                expandAzureADGroup -objectid $azroas.ObjectID -PipelineVariable azadg | select `
+                    @{Name="SubscriptionID";Expression={$azco.subscription}}, `
+                    @{Name="TenantId";Expression={$Azco.Tenant}}, `
+                    @{Name="ResourceID";Expression={$azroas.role}}, `
+                    @{Name="RoleDefinitionID";Expression={$azroas.RoleDefinitionID}}, `
+                    @{Name="RoleDefinitionName";Expression={$azrole}}, `
+                    @{Name="Group";Expression={$azroas.Displayname}}, `
+                    Displayname,signinname,objectid,ObjectType
+            }else{        
+                $_ | select `
+                    @{Name="SubscriptionID";Expression={$azco.subscription}}, `
+                    @{Name="TenantId";Expression={$Azco.Tenant}}, `
+                    @{Name="ResourceID";Expression={$_.role}}, `
+                    RoleDefinitionID, `
+                    @{Name="RoleDefinitionName";Expression={$azrole}}, `
+                    @{Name="Group";Expression={}}, `
+                    Displayname,signinname,
+                    @{Name="ObjectID";Expression={if($azroas.ObjectID){$azroas.ObjectID}else{resolveAzureRoleObject -name $azroas.SignInName -objecttype $azroas.objecttype}}}, `
+                    ObjectType
+                }
+            }
+        }
+    }
+}#>
+
+connect-azaccount
+Launcher
+write-host "Results can be found here $report"
