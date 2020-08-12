01f0c4a8b0a93547b70cdb563c9605cf59202939
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-08-12 10:53:14 -0700

    Update AzureADDeviceScriptCollection.ps1

--- Maintenance Task/AzureADDeviceScriptCollection.ps1
+++ Maintenance Task/AzureADDeviceScriptCollection.ps1
@@ -1,4 +1,4 @@
-#this will list all Windows devices
+ta#this will list all Windows devices
 get-azureaddevice -Filter "DeviceOSType eq 'Windows'" -all $true | select `
     objectid, deviceid, DisplayName,AccountEnabled,ApproximateLastLogonTimeStamp,DeviceOSType,DeviceOSVersion, `
         DeviceTrustType,DirSyncEnabled,LastDirSyncTime,ProfileType | export-csv .\aad_device_windows.csv -NoTypeInformation
@@ -13,7 +13,7 @@ get-azureaddevice -Filter "DeviceTrustType eq 'ServerAD'" -all $true | select `
     objectid, deviceid, DisplayName,AccountEnabled,ApproximateLastLogonTimeStamp,DeviceOSType,DeviceOSVersion, `
         DeviceTrustType,DirSyncEnabled,LastDirSyncTime,ProfileType | export-csv .\aad_device_hybridjoined.csv -NoTypeInformation
 
-#this will list all direc azure ad joined devices
+#this will list all direct azure ad joined devices
 get-azureaddevice -Filter "DeviceTrustType eq 'AzureAD'" -all $true | select `
     objectid, deviceid, DisplayName,AccountEnabled,ApproximateLastLogonTimeStamp,DeviceOSType,DeviceOSVersion, `
         DeviceTrustType,DirSyncEnabled,LastDirSyncTime,ProfileType | export-csv .\aad_device_aadjoined.csv -NoTypeInformation
@@ -37,11 +37,12 @@ get-azureaddevice -all $true | where {$_.AccountEnabled -eq $false} | select `
 $dt = [datetime]'2020/01/01'
 get-azureaddevice -all $true | where {$_.ApproximateLastLogonTimeStamp -le $dt -and $_.AccountEnabled -eq $false} | Remove-AzureADDevice
 
-#using MSOL module - list devices not really hybrid device joined 
+#using MSOL module - list devices hybrid device joined in pending state 
+#https://docs.microsoft.com/en-us/azure/active-directory/devices/hybrid-azuread-join-manual#using-powershell
 Get-MsolDevice -All -IncludeSystemManagedDevices | `
   where {($_.DeviceTrustType -eq 'Domain Joined') -and (-not([string]($_.AlternativeSecurityIds)).StartsWith("X509:"))} | select `
     objectid, deviceid, displayname, enabled, DeviceOSVersion, DeviceTrustType,DirSyncEnabled,LastDirSyncTime,ApproximateLastLogonTimeStamp | `
-        export-csv .\aad_device_aadjoined.csv -NoTypeInformation
+        export-csv .\aad_device_pendinghybridjoin.csv -NoTypeInformation
     
 #using MSOL module - Get Stale Devices
 Get-MsolDevice -All -LogonTimeBefore 'January 1, 2020 12:00:00 AM' | select `
