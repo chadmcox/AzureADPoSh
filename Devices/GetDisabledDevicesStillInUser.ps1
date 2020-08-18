afa06c4fba66451402075453cbb38164db948103
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-08-18 09:25:34 -0700

    Update and rename GetDisabledDevicesStillInUser.ps1 to GetDisabledAADDevicesStillInUser.ps1

--- Devices/GetDisabledDevicesStillInUser.ps1
+++
@@ -1,8 +0,0 @@
-#Requires -module azureadpreview
-connect-azuread
-
-#this will list devices that are still being used but are disabled
-Get-AzureADAuditSignInLogs -filter "appDisplayName eq 'Microsoft Office' and status/errorCode eq 135011" -all $true | select `
-    UserPrincipalName,AppDisplayName, @{Name="DeviceName";Expression={$_.DeviceDetail.DisplayName}}, `
-    @{Name="ErrorCode";Expression={$_.Status.errorcode}}, @{Name="FailureReason";Expression={$_.Status.FailureReason}} -Unique | `
-        export-csv .\aad_device_still_used_but_disabled.csv
