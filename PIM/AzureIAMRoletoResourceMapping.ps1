9f1fb5bad31afed289489ade393bbd63b9e3a9b0
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-09-25 16:12:27 -0700

    Update AzureIAMRoletoResourceMapping.ps1

--- PIM/AzureIAMRoletoResourceMapping.ps1
+++ PIM/AzureIAMRoletoResourceMapping.ps1
@@ -156,7 +156,7 @@ if(check-file -file $pim_File){
                          @{Name="Displayname";Expression={$member.DisplayName}}, `
                          @{Name="SigninName";Expression={$member.userprincipalname}}, `
                          @{Name="AssignmentState";Expression={if($azpra.AssignmentState -like "Active" -and $azpra.EndDateTime -eq $null) `
-                          {"$($azpra.AssignmentState) - Permanant"}elseif($azpra.AssignmentState -like "Active" -and $azpra.EndDateTime -like "*") `
+                          {"$($azpra.AssignmentState) - Permanent"}elseif($azpra.AssignmentState -like "Active" -and $azpra.EndDateTime -like "*") `
                           {"$($azpra.AssignmentState) - Elevated"}else{$azpra.AssignmentState}}}, `
                          @{Name="AssignmentType";Expression={"PrivilegedRoleAssignment"}}
             }
