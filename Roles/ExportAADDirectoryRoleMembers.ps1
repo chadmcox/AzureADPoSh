8f4f08f11e4d7b0f7300933447918aed56009110
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-12-03 08:31:42 -0700

    Update ExportAADDirectoryRoleMembers.ps1

--- Roles/ExportAADDirectoryRoleMembers.ps1
+++ Roles/ExportAADDirectoryRoleMembers.ps1
@@ -1,6 +1,8 @@
 #Requires -Modules AzureADPreview
 <#
   This script will just export all members out of every role in Azure AD.
+  if use pim consider using the PIM related script this will not show elgible
+  assignments
 #>
 
 Param($report="$env:userprofile\Documents\AzureAD_Directory_Role_Members.csv")
