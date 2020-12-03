16ce108f01c0071d9c085dc0b8729e3a46d97264
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-12-03 08:34:33 -0700

    Update and rename reviewAADPIMRoleMem.ps1 to exportAADPIMRoleMembers.ps1

--- PIM/exportAADPIMRoleMembers.ps1
+++ PIM/exportAADPIMRoleMembers.ps1
@@ -1,4 +1,12 @@
+#Requires -Modules AzureADPreview
+<#
+  This script will export all members out of every role in Azure PIM. Use this if you are using PIM if not use
+  https://raw.githubusercontent.com/chadmcox/Azure_Active_Directory_Scripts/master/Roles/ExportAADDirectoryRoleMembers.ps1
+#>
 Param($report="$env:userprofile\Documents\AADPIMRoleMembers.csv")
+
+#check to see if already logged into AAD prompt if not
+if(!((Get-AzureADTenantDetail).objectid)){connect-azuread}
 
 #get all privilaged role membersrole members
 Get-AzureADMSPrivilegedRoleDefinition -ProviderId "aadRoles" -ResourceId (Get-AzureADTenantDetail).objectid -pv role | foreach{
