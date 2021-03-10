8549fb6e81bffe20944cfcce902eb7315c98a5fe
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-03-10 14:17:02 -0700

    Update exportConditionalAccessPolicytoCSV.ps1

--- Conditional Access Policy/exportConditionalAccessPolicytoCSV.ps1
+++ Conditional Access Policy/exportConditionalAccessPolicytoCSV.ps1
@@ -7,9 +7,12 @@ param($exportfile=".\conditional_access_policy_export.csv")
 
 function resolveguid{
     param($in,$cast)
-    #write-host $cast
-    if($in -match "(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$"){
-        try{(Get-AzureADObjectByObjectId -ObjectIds $in).displayname}catch{$in}
+    
+    if($in -match "(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$"){$results = @()
+        $in -split " " | foreach{
+        $results += try{(Get-AzureADObjectByObjectId -ObjectIds $_).displayname}catch{$_}}
+        if(!($results)){$results = $in -split " "}
+        return $results
     }elseif(($in | get-member)[0].TypeName -eq "system.int32"){$results = @()
         $in -split " " | foreach{$results = 
         $results += invoke-expression "[$cast].GetEnumName([int]$_)"}
@@ -24,6 +27,7 @@ function resolveguid{
 function expandcap{
     param($objcap,$space,$cast)
     if($objcap){
+    
     #($objcap | get-member)[0].TypeName
     if(($objcap | get-member)[0].TypeName -notlike "Microsoft.Open.MSGraph.Model.*"){
         if($space -eq "/Id"){$hash_prop.add($space,$objcap)}else{
@@ -31,10 +35,9 @@ function expandcap{
         }
     }Else{
         $objcap | get-member -membertype Property | foreach{
-            #"$space" + "$($_.name)"
-            #($objcap).($_.name)   #comment this out
-            #$hash_prop.add($("$space" + "/" + "$($_.name)"),$null)
-            if("$space" + "/" + "$($_.name)" -eq "/GrantControls/BuiltInControls"){
+            
+            if("$space" + "/" + "$($_.name)" -eq "/GrantControls/BuiltInControls" -and $(($objcap).($_.name)) -eq "Block"){
+            
             $hash_prop.add($("$space" + "/" + "$($_.name)"),$(($objcap).($_.name)))
             }
             expandcap -objcap ($objcap).($_.name) -space $("$space" + "/" + "$($_.name)") -cast ($objcap | get-member)[0].TypeName
@@ -45,7 +48,7 @@ function expandcap{
 }
 
 #Get-AzureADMSConditionalAccessPolicy -PolicyId 4998f8af-e8c5-4ab2-b598-c33e96e12ab8 -pv cap  | foreach{
-$conditional_access_Policies = Get-AzureADMSConditionalAccessPolicy -pv cap  | foreach{
+$conditional_access_Policies = Get-AzureADMSConditionalAccessPolicy  -pv cap  | foreach{
 $hash_prop = @{}
 write-host "Dumping $($cap.displayname)"
 expandcap -objcap $cap -space $null
@@ -55,5 +58,5 @@ new-object psobject -property $hash_prop
 
 $unique_props = $conditional_access_Policies | foreach{$_ | get-member -MemberType NoteProperty} | select name -unique
 $unique_props = @("/Id","/DisplayName","/State") + ($unique_props | where {$_.name -notin "/Id","/DisplayName","/State"} | sort name).name
+
 $conditional_access_Policies | select $unique_props | export-csv $exportfile -NoTypeInformation
-write-host "Results can be found here $exportfile"
