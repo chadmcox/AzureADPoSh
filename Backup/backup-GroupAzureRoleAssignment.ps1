95a5d0e491824f9e6d5e380ec5ca629e1185416d
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-04-08 16:30:13 -0700

    Update backup-GroupAzureRoleAssignment.ps1

--- Backup/backup-GroupAzureRoleAssignment.ps1
+++ Backup/backup-GroupAzureRoleAssignment.ps1
@@ -1,1 +1,16 @@
-
+Get-AzSubscription -pv sub | set-azcontext | foreach{
+    write-host "Enumerating: $($sub.name)"
+    Get-AzRoleAssignment -IncludeClassicAdministrators -pv assignment | foreach{
+        foreach($rn in ($assignment.RoleDefinitionName -split ";")){
+            $_ | select @{Name="Scope";Expression={$assignment.Scope}}, `
+                @{Name="ScopeType";Expression={($assignment.Scope -split "/")[-2]}}, `
+                @{Name="RoleDefinitionName";Expression={$rn}}, `
+                @{Name="RoleDefinitionId";Expression={$assignment.RoleDefinitionId}}, `
+                @{Name="DisplayName";Expression={$assignment.DisplayName}}, `
+                @{Name="SignInName";Expression={$assignment.SignInName}}, `
+                @{Name="ObjectID";Expression={$assignment.ObjectID}}, `
+                @{Name="ObjectType";Expression={$assignment.Objecttype}}, `
+                @{Name="Subscription";Expression={"$($sub.name) - ($sub.id)"}}
+        }
+    } 
+} | export-csv ".\Azure_Role_Assignment_$((Get-AzureADTenantDetail).DisplayName)_$(get-date -f yyyy-MM-dd).csv" -NoTypeInformation
