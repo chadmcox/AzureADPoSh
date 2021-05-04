7dfa5b9d01afbfa6d3de6910ae414b2ddf74df25
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-05-04 13:06:22 -0700

    Update getSyncAccountSigninApps.ps1

--- Azure AD Connect/getSyncAccountSigninApps.ps1
+++ Azure AD Connect/getSyncAccountSigninApps.ps1
@@ -1,3 +1,6 @@
+try{Get-AzureADCurrentSessionInfo}
+catch{Connect-azuread | out-null}
+
 #this shows apps this account should not be logging into
 Get-AzureADDirectoryRole -Filter "RoleTemplateId eq 'd29b2b05-8046-44ba-8758-1e26182fcf32'" | Get-AzureADDirectoryRoleMember -pv mem | foreach{
     write-host "Getting logs for $($mem.userprincipalname)"
