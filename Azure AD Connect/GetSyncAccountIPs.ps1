7c58829b900308ce4fa4e3f11b7d80b110eb4f51
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-05-04 13:06:00 -0700

    Update GetSyncAccountIPs.ps1

--- Azure AD Connect/GetSyncAccountIPs.ps1
+++ Azure AD Connect/GetSyncAccountIPs.ps1
@@ -1,5 +1,5 @@
 try{Get-AzureADCurrentSessionInfo}
-catch{Connect-azuread}
+catch{Connect-azuread | out-null}
 
 Get-AzureADDirectoryRole -Filter "RoleTemplateId eq 'd29b2b05-8046-44ba-8758-1e26182fcf32'" | Get-AzureADDirectoryRoleMember -pv mem | foreach{
     write-host "Getting logs for $($mem.userprincipalname)"
