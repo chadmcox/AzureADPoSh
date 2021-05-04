5801db2c4c941b2f93df1df25e508806a7ef8672
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-05-04 11:39:45 -0700

    Create GetSyncAccountIPs.ps1

---
+++ Azure AD Connect/GetSyncAccountIPs.ps1
@@ -0,0 +1,5 @@
+Get-AzureADDirectoryRole -Filter "RoleTemplateId eq 'd29b2b05-8046-44ba-8758-1e26182fcf32'" | Get-AzureADDirectoryRoleMember -pv mem | foreach{
+    write-host "Getting logs for $($mem.userprincipalname)"
+    try{Get-AzureADAuditSignInLogs -Filter "userId eq '$($mem.objectid)'" -all $true | select UserPrincipalName, IpAddress}catch{}
+    Start-Sleep -Seconds 3
+} | select UserPrincipalName, IpAddress -unique
